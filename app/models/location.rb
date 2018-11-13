200d2ae3ada8ff5940dd57155e537a2569e73907
Lewis Cruz <bluelew75@gmail.com>
2018-11-13 01:38:08 -0500

    Added sticky search bar on home page.

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,15 @@
 class Location < ApplicationRecord
+  geocoded_by :address
+  after_validation :geocode, if: :address_changed?
+
+  def address
+    [city, state, country].compact.join(", ")
+  end
+
+  def address_changed?
+    city_changed? || state_changed? || country_changed?
+  end
+
   has_many :user_locations
   has_many :users, through: :user_locations
 end
